c72ca5fb0000c86718b860739dbe88fd34c2b6da
WladimirFilho <72136799+WladimirFilho@users.noreply.github.com>
2023-09-16 20:40:07 +1000

    Loop over articles

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 
 
 async function main() {
-    // Prims Queries
+    // Prisma Queries
 
     // Create User
     // const user = await prisma.user.create({
@@ -15,24 +15,81 @@ async function main() {
     // })
     //  console.log(user);
 
+    ////////////////////////////////
+
     // Get all users
-    // const users = await prisma.user.findMany()
-    // console.log(users);
-    
-    // Create article and associated with the user
-    const article = await prisma.article.create({
-        data: {
-            title: "John First Article",
-            body: "This is Johns first article",
-            author: {
-                connect: {
-                    id: 1
-                }
-            }
+    const users = await prisma.user.findMany({
+        include: {
+            articles: true
         }
     })
+    // console.log(users);
 
-    console.log(article)
+      ////////////////////////////////
+    
+    // Create article and associated with the user
+    // const article = await prisma.article.create({
+    //     data: {
+    //         title: "John First Article",
+    //         body: "This is Johns first article",
+    //         author: {
+    //             connect: {
+    //                 id: 1
+    //             }
+    //         }
+    //     }
+    // })
+    // console.log(article)
+
+    // Get all articles
+    // const allArticles = await prisma.article.findMany()
+    // console.log(allArticles)
+
+    // Create User and article and associate them together
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: "Sarah Smith",
+    //         email: "sara@gmail.com",
+    //         articles: {
+    //             create: {
+    //                 title: "Saras First Article",
+    //                 body: "This is Saras first article"
+    //             }
+    //         }
+    //     }
+    // })
+    // console.log(user)
+
+    // const articles = await prisma.article.findMany()
+    // console.log(articles)
+
+    ////////////////////////////////
+
+    // Create another article
+    // const article = await prisma.article.create({
+    //     data: {
+    //         title: "Sample article",
+    //         body: "Sample article",
+    //         author: {
+    //             connect: {
+    //                 id: 2
+    //             }
+    //         }
+    //     }
+    // })
+    // console.log(article)
+
+    ////////////////////////////////////////////////////////
+
+    // Loop over articles
+    users.forEach((user) => {
+        console.log(`User: ${user.name} Email: ${user.email}`)
+        console.log("Article:")
+        user.articles.forEach((article) => { 
+            console.log(`- Title: ${article.title} Body: ${article.body}`)
+        })
+        console.log("\n")
+    })
 }
 
 main()
